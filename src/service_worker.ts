b520feb9dfa92756279db1f62ebf45d59b40cee6
Rishav Rout <rout.rishav@gmail.com>
2024-09-12 13:36:22 -0700

    ts conversion

---
+++ src/service_worker.ts
@@ -0,0 +1,19 @@
+const huh = chrome.commands.onCommand.addListener((command) => {
+    if (command === "pencil") {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            const tabId = tabs[0].id;
+            if (tabId) {
+                chrome.tabs.sendMessage(tabId, {action: "click-pencil"});
+            }
+        });
+    }
+    if (command === "pause") {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            const tabId = tabs[0].id;
+            if (tabId) {
+                chrome.tabs.sendMessage(tabId, {action: "pause-unpause"});
+            }
+        });
+    }
+});
+chrome.commands.getAll().then(huh => console.log(huh));
